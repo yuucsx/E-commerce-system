a120128163cf125ea8413b56e71da37cf428b8a0
Yuu <yuu.csx@gmail.com>
2022-10-17 23:07:16 -0300

    xdd

--- sistema E-commerce/Sistema_E-commerce/delivery/settings.py
+++ sistema E-commerce/Sistema_E-commerce/delivery/settings.py
@@ -84,8 +84,15 @@ WSGI_APPLICATION = 'delivery.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sistema',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': '',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command' :  'SET default_storage_engine=InooDB'
+        }
     }
 }
 
